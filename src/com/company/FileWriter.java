5fb35890309ad65d81459a2de1316bb52e882a77
Anders Knudsen <anderskdknudsen@gmail.com>
2016-11-28 14:24:22 +0100

    filewriter burde virke sådan lidt.

--- src/com/company/FileWriter.java
+++ src/com/company/FileWriter.java
@@ -32,4 +32,42 @@ public class FileWriter {
 
         }
     }
+
+    public void readFile(ArrayList<Membership> members) throws IOException{
+
+        Scanner input = new Scanner(new File(fileName));
+        input.nextLine();
+        while(input.hasNextLine()){
+            String text = input.nextLine();
+            readLine(text, members);
+        }
+    }
+
+    public void readLine(String text, ArrayList<Membership> members){
+
+        Scanner lineScanner = new Scanner(text);
+        int tempID;
+        String tempName;
+        int tempAge;
+        boolean tempActive;
+        boolean tempElite;
+        boolean tempJunior;
+        boolean tempSeniorDisc;
+        int tempPrice;
+
+
+        while(lineScanner.hasNext()){
+            tempID = lineScanner.nextInt();
+            tempName = lineScanner.next();
+            tempAge = lineScanner.nextInt();
+            tempActive = lineScanner.nextBoolean();
+            tempElite = lineScanner.nextBoolean();
+            tempJunior = lineScanner.nextBoolean();
+            tempSeniorDisc = lineScanner.nextBoolean();
+            tempPrice = lineScanner.nextInt();
+
+            members.add(new Membership());
+
+        }
+    }
 }
